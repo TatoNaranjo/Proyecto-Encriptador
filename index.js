b8e7d19ee87ab3116ccc76fb3c2840f42dfbb95d
SANTIAGO NARANJO HERRERA <snaranjoh@ucundinamarca.edu.co>
2024-01-23 23:29:52 -0500

    Fix Deployment Bugs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3030;
 
-// your code
+console.log("Hello World")
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
